d8bb4bf90b7f2c448691f3c5b0b1e0f9eb919eb4
piotr.blaszczak <piotr.blaszczak@nf.pl>
2014-10-29 13:03:00 +0100

    config fix

--- app/templates/public-tpls/scripts/config/config.js
+++ app/templates/public-tpls/scripts/config/config.js
@@ -1,18 +1,22 @@
 'use strict';
 
 angular.module('<%= scriptAppName %>')
-    .constant('Config', angular.deepExtend({
-        viewsDir: 'views/',
-        componentsDir: 'components/',
-        statesDir: 'states/',
-        environment: 'production', //development or production
-        API: {
-            protocol: window.location.protocol.split(':')[0], //Use the same protocol, host and port as the UI is hosted from bu default
-            host: window.location.hostname,
-            port: String(window.location.port || 80),
-            path: '/api'
-        }
-    }, angular._localConfig || {}))
+    .service('config', function(){
+        var config = angular.deepExtend({
+            viewsDir: 'views/',
+            componentsDir: 'components/',
+            statesDir: 'states/',
+            environment: 'production', //development or production
+            api: {
+                protocol: window.location.protocol.split(':')[0], //Use the same protocol, host and port as the UI is hosted from bu default
+                host: window.location.hostname,
+                port: String(window.location.port || 80),
+                path: '/api'
+            }
+        }, angular._localConfig || {});
+        angular.extend(config, window.__config||{user:{id:null}});
+        return config;
+    })
     .value('cgBusyDefaults',{
         message:'Loading...',
         backdrop: true,
@@ -20,9 +24,9 @@ angular.module('<%= scriptAppName %>')
         delay: 0,
         minDuration: 500
     })
-    .factory('BaseUrl', function (Config) {
-        return (Config.API.protocol + '://' + Config.API.host + ':' + Config.API.port + '/');
+    .factory('baseUrl', function (config) {
+        return (config.api.protocol + '://' + config.api.host + ':' + config.api.port + '/');
     })
-    .factory('APIBaseUrl', function (Config) {
-        return (Config.API.protocol + '://' + Config.API.host + ':' + Config.API.port + Config.API.path + '/');
+    .factory('apiBaseUrl', function (config) {
+        return (config.api.protocol + '://' + config.api.host + ':' + config.api.port + config.api.path + '/');
     });
