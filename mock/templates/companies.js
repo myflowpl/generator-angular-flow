a3b450ac1c749588ea81bab545bf289675b36d58
myflowpl <pedro.blaszczak@gmail.com>
2015-04-24 16:56:02 +0200

    mocks generator temlates

---
+++ mock/templates/companies.js
@@ -0,0 +1,14 @@
+var nf_mocks = window.nf_mocks || {};
+
+nf_mocks.companies = [
+    {
+        id: 1,
+        name: 'test 1'
+    },
+    {
+        id: 2,
+        name: 'test 345'
+    }
+];
+
+nf_mocks.companies_pk = nf_mocks.companies.length;
